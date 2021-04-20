73c994c870a62a96c4a5fa946cdc3821376f02ad
Hudson Carolino <hudson@kenzie.com.br>
2021-04-19 21:45:15 -0300

    Estruturando html front-end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="home">
-    <h1>App</h1>
+    <div class="app_content">
+      <h2>Adicionar novas tarefa</h2>
+      <div class="app_content__formulario">
+        <input type="text" placeholder="Adicionar nova tarefa">
+        <textarea placeholder="Descrição Tarefa" ></textarea>
+        <button>Adicionar</button>
+
+        <h3>Lista de tarefas</h3>
+        <ul>
+          <li>
+            <span>X</span>
+            <h1>Nome Tarefa</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="app_content">
+      <h2>Tarefas concluidas</h2>
+      <ul>
+        <li>
+            <span>X</span>
+            <h1>Nome Tarefa</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+      </ul>
+    </div>
   </div>
 </template>
 

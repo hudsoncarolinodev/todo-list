49cbd3f6ef20d02a4a511a9fc43d8233fe8d50a1
Hudson Carolino <hudson@kenzie.com.br>
2021-04-19 22:53:58 -0300

    aplicando estilo no layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,27 @@
         <input type="text" placeholder="Adicionar nova tarefa">
         <textarea placeholder="Descrição Tarefa" ></textarea>
         <button>Adicionar</button>
-
-        <h3>Lista de tarefas</h3>
-        <ul>
-          <li>
-            <span>X</span>
-            <h1>Nome Tarefa</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-          </li>
-        </ul>
       </div>
+      <h3>Lista de tarefas</h3>
+      <ul>
+        <li>
+          <span>X</span>
+          <h1>Nome Tarefa</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </li>
+          <li>
+          <span>X</span>
+          <h1>Nome Tarefa</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </li>
+          <li>
+          <span>X</span>
+          <h1>Nome Tarefa</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </li>
+      </ul>
     </div>
+   
     <div class="app_content">
       <h2>Tarefas concluidas</h2>
       <ul>
